55af0a473277912079f3914534f130a9c8aded1e
RLCHuncp <rlch06.uncp@gmail.com>
2020-08-20 19:11:04 -0500

    Updates

---
+++ precache-manifest.242ac95a22badf38bb0a6d49ff5d09c4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a9dc5d8845f6561a62a89ab79d8b18",
+    "url": "/LIM012-fe-burger-queen/index.html"
+  },
+  {
+    "revision": "706d03ba374205f6f3d5",
+    "url": "/LIM012-fe-burger-queen/static/css/main.e4258708.chunk.css"
+  },
+  {
+    "revision": "77eac873407bb4af2011",
+    "url": "/LIM012-fe-burger-queen/static/js/2.47acf9ae.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/LIM012-fe-burger-queen/static/js/2.47acf9ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706d03ba374205f6f3d5",
+    "url": "/LIM012-fe-burger-queen/static/js/main.5403de87.chunk.js"
+  },
+  {
+    "revision": "f738eb78eccef1edea7a",
+    "url": "/LIM012-fe-burger-queen/static/js/runtime-main.30c4dd00.js"
+  },
+  {
+    "revision": "c5b63b1081ac5c876a04e8149cd7786c",
+    "url": "/LIM012-fe-burger-queen/static/media/add.c5b63b10.svg"
+  },
+  {
+    "revision": "5119c53cc2261bd4634e86523f54baec",
+    "url": "/LIM012-fe-burger-queen/static/media/burger.5119c53c.png"
+  },
+  {
+    "revision": "3737a646f7dc983d8e9e4bfc4e1af6ab",
+    "url": "/LIM012-fe-burger-queen/static/media/delete.3737a646.svg"
+  },
+  {
+    "revision": "3aa30e5dd3838a4c137820c8648d0906",
+    "url": "/LIM012-fe-burger-queen/static/media/waiter.3aa30e5d.svg"
+  }
+]);
