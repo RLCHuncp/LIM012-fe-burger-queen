0e4bda86e2c410d113db720e58d65de988daf816
RLCHuncp <rlch06.uncp@gmail.com>
2020-08-26 22:17:35 -0500

    --1--

--- src/__test__/Box-Pedidos.test.js
+++ src/__test__/Box-Pedidos.test.js
@@ -1,1 +1,18 @@
 import React from 'react';
+import BoxPedidos from '../components/Box-Pedidos';
+import { render, getAllByTestId, getNodeText } from '@testing-library/react';
+
+describe('<BoxPedidos />', () => {
+    it('Debería mostrar la lista de pedidos marcados', () => {
+        const eliminar = jest.fn();
+        const listProduct = {
+            Descripcion: 'Hamburguesa doble',
+            Precio: 15, 
+            Cantidad: 2,
+            id: 'AeYaJUiQbMhKGjEVblwu',
+        };
+        const container = render(<BoxPedidos order={listProduct} ProductItemOrder={eliminar}/>);
+        const listaDeNodos = container.getAllByTestId('item');
+
+    })
+})
